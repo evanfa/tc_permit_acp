5a745c601ef59bb8d47408b08d45ad239df082c6
start.vaadin.com <no-reply@vaadin.com>
2022-11-10 22:23:20 +0000

    Generated project

---
+++ src/main/java/com/tc/permt/application/data/Role.java
@@ -0,0 +1,5 @@
+package com.tc.permt.application.data;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/com/tc/permt/application/data/service/SamplePersonRepository.java
@@ -0,0 +1,9 @@
+package com.tc.permt.application.data.service;
+
+import com.tc.permt.application.data.entity.SamplePerson;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SamplePersonRepository extends JpaRepository<SamplePerson, UUID> {
+
+}
